521b6d840b0a69532e9d31ba2a438cf077cef5a9
swagata-datta <93482143+swagata-datta@users.noreply.github.com>
2022-12-02 12:23:27 -0500

    day-2 complete

--- day-2.py
+++ day-2.py
@@ -28,8 +28,31 @@ def calculate_score(input_f):
 
 assert calculate_score(inp_test) == 15
 
-print(calculate_score(inp_day2))
+#print('Part 1:', calculate_score(inp_day2))
+
+## part 2
 
 
+def calculate_part2(input_f):
+    """Calculates"""
+    # creating three lists for win, draw, and loss
+    win_list = [x[0] for x in input_f if x[-1] == 'Z']
+    draw_list = [x[0] for x in input_f if x[-1] == 'Y']
+    loss_list = [x[0] for x in input_f if x[-1] == 'X']
+
+    # for wins, if opponent plays rock, I play paper and get 2 points, paper implies 3 points and so on
+    win_dic = {'A': 2, 'B':3, 'C':1}
+    win_shape = [win_dic[x] for x in win_list] # list of points for the wins
+    draw_dic = {'A': 1, 'B':2, 'C':3}
+    draw_shape = [draw_dic[x] for x in draw_list]
+    # for loss, if opponet plays rock, I play scissor and get 3 points, paper implies 1 points and so on
+    loss_dic = {'A': 3, 'B':1, 'C':2}
+    loss_shape = [loss_dic[x] for x in loss_list]
+
+    score = 6*len(win_list) + 3*len(draw_list) + sum(win_shape) + sum(loss_shape) + sum(draw_shape)
+    return score
+
+assert calculate_part2(inp_test) == 12
+print('Part 2: ',calculate_part2(inp_day2))
 
 
