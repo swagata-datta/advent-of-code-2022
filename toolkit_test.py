ed24b8c5f1c3a53ac14973e65a5d3a073ea1286d
swagata-datta <93482143+swagata-datta@users.noreply.github.com>
2023-01-14 21:43:34 -0700

    Update toolkit_test.py

--- toolkit_test.py
+++ toolkit_test.py
@@ -16,9 +16,30 @@ assert str_to_tup('3, 4') == (3,4)
 assert str_to_tup('4,5,6') == (4,5,6)
 
 
-x = [1,1,2,3,4]
+# decimal to binary
 
-print(len(set(x)))
+def decimal_to_binary(num, bits, indices = []):
+    """Takes in a decimal number, and number of bits and returns the binary"""
+    def largest_exponent(num):
+        """Returns the largest possible exponent of two that goes into the number
+        e.g. for 7, the largest possible is 2^2=4. The function will return 2"""
+        for i in range(bits):
+            if num < 2:
+                return i
+            elif num / 2 < 2:
+                return i + 1
+            else:
+                num = num / 2
+    if num == 0:
+        binary = ['0'] * bits
+        for i in indices:
+            binary[i] = '1'
+        binary.reverse()
+        final = ''.join(binary)
+        return final
+    else:
+        exp = largest_exponent(num)                                 # the largest exponent of 2 that `goes into` num
+        indices.append(exp)  
+        num = num - 2 ** exp                                        # new number is what remains
+        return decimal_to_binary(num, bits, indices)
 
-for i in range(1):
-    print(i)
